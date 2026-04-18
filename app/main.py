b1b34e33e13e4ba3c786ed15ef875afebbfda6ae
katyakhlystova <ya_zvizda12@mail.ru>
2026-04-18 19:07:24 +0700

    added login and logout with session

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.middleware.sessions import SessionMiddleware
 
 from app.db import engine, Base, get_session
 from app.models.user import User
@@ -10,6 +11,8 @@ from app.schemas.user import UserCreate
 from app.services.user_service import UserService
 
 app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key="super-secret-key")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -24,10 +27,16 @@ async def startup():
 # главная страница
 @app.get("/")
 def main_page(request: Request):
+    user_name = request.session.get("user_name")
+    user_email = request.session.get("user_email")
+
     return templates.TemplateResponse(
         request=request,
         name="index.html",
-        context={}
+        context={
+            "user_name": user_name,
+            "user_email": user_email
+        }
     )
 
 
@@ -63,7 +72,7 @@ async def register_user(
 
         await user_service.register_user(user_data)
 
-        return RedirectResponse(url="/", status_code=303)
+        return RedirectResponse(url="/login", status_code=303)
 
     except ValueError as e:
         return templates.TemplateResponse(
@@ -71,3 +80,43 @@ async def register_user(
             name="register.html",
             context={"error": str(e)},
         )
+
+
+@app.get("/login")
+def login_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html",
+        context={"error": None}
+    )
+
+
+@app.post("/login")
+async def login_user(
+    request: Request,
+    email: str = Form(...),
+    password: str = Form(...),
+    session: AsyncSession = Depends(get_session),
+):
+    user_repository = UserRepository(session)
+
+    user = await user_repository.get_by_email(email)
+
+    if user is None or user.password != password:
+        return templates.TemplateResponse(
+            request=request,
+            name="login.html",
+            context={"error": "Неверный email или пароль"},
+        )
+
+    request.session["user_id"] = user.id
+    request.session["user_name"] = f"{user.first_name} {user.last_name}"
+    request.session["user_email"] = user.email
+
+    return RedirectResponse(url="/", status_code=303)
+
+
+@app.get("/logout")
+def logout_user(request: Request):
+    request.session.clear()
+    return RedirectResponse(url="/", status_code=303)
