5573c3fb5cc67fb30d54a6b3da59cddc9b06e5f2
katyakhlystova <ya_zvizda12@mail.ru>
2026-04-18 14:35:48 +0700

    first working page

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def main_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
